78e87b564dac5dc6fa9766aea8aae5ca141f3308
StevenMiraba <150202191+StevenMiraba@users.noreply.github.com>
2025-07-24 16:45:10 -0500

    corrections

--- src/types/DashboardTypes.tsx
+++ src/types/DashboardTypes.tsx
@@ -1,41 +1,29 @@
-export interface OpenMeteoResponse {
-  latitude: number;
-  longitude: number;
-  generationtime_ms: number;
-  utc_offset_seconds: number;
-  timezone: string;
-  timezone_abbreviation: string;
-  elevation: number;
-  hourly_units: HourlyUnits;
-  hourly: Hourly;
-  current: Current;
-  current_units: CurrentUnits;
-}
-
-export interface Hourly {
-  time: string[];
-  temperature_2m: number[];
-  relative_humidity_2m: number[];
-}
-
-export interface HourlyUnits {
-  time: string;
-  temperature_2m: string;
-  relative_humidity_2m: string;
-}
-
-export interface Current {
-  time?: string;
-  temperature_2m?: number;
-  apparent_temperature?: number;
-  wind_speed_10m?: number;
-  relative_humidity_2m?: number;
+export interface Root {
+  latitude: number
+  longitude: number
+  generationtime_ms: number
+  utc_offset_seconds: number
+  timezone: string
+  timezone_abbreviation: string
+  elevation: number
+  current_units: CurrentUnits
+  current: Current
 }
 
 export interface CurrentUnits {
-  time?: string;
-  temperature_2m?: string;
-  apparent_temperature?: string;
-  wind_speed_10m?: string;
-  relative_humidity_2m?: string;
+  time: string
+  interval: string
+  temperature_2m: string
+  apparent_temperature: string
+  wind_speed_10m: string
+  relative_humidity_2m: string
+}
+
+export interface Current {
+  time: string
+  interval: number
+  temperature_2m: number
+  apparent_temperature: number
+  wind_speed_10m: number
+  relative_humidity_2m: number
 }
